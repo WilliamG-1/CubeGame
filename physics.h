1c0b383e3a846f753033ef3dc9f1a3f655a8dd4f
William <williamgomez32375@gmaill.com>
2022-12-05 16:35:07 -0800

    Added Physics Abstract Class

--- physics.h
+++ physics.h
@@ -1,6 +1,19 @@
 #pragma once
-
+#include "glm/gtc/matrix_transform.hpp"
 class Physics
 {
+public:
+    virtual void applyGravity(float dt) = 0;
+    float Gravity() { return gravity;}
 
+protected:
+    float velocity = 0;
+    float x;
+    float y;
+    float xVelocity = 0;
+    float yVelocity = 0;
+    float mass = 0;
+
+private:
+    float gravity = -9.8f;
 };
