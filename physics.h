05665361c2cb6e17e64272c3d5f83536517828e3
William <williamgomez32375@gmaill.com>
2023-01-02 23:31:11 -0600

    Physics Components (Friction, force, acceleration)

--- physics.h
+++ physics.h
@@ -17,20 +17,33 @@ class Physics
 {
 public:
     virtual void applyGravity(float dt) = 0;
-
-    void cancel_x_velocity() { velocity.x = 0; }
+    // Simulate "Friction" so that object doesn't move forever
+    virtual void update_horizontal_movement(float dt) = 0;
+    
+    void cancel_x_force() { force.x = 0; }
     void cancel_y_velocity() { velocity.y = 0; }
-    void cancel_z_velocity() { velocity.z = 0; }
-    glm::vec3 get_velocity() const { return velocity; }
+    void cancel_z_force() { force.z = 0; }
+    virtual void not_applying_left_force() = 0;
+    virtual void not_applying_right_force() = 0;
 
+    glm::vec3 get_velocity() const { return velocity; }
+    glm::vec3 get_acceleration() const { return acceleration; }
+    glm::vec3 get_force() const { return force; }
     float Gravity() { return gravity;}
 protected:
     glm::vec3 velocity = {0, 0, 0};
+    glm::vec3 force = {0, 0, 0};
+    glm::vec3 friction_force = {0, 0, 0};
+    glm::vec3 acceleration = {0, 0, 0};
     float x;
     float y;
     float z;
     float mass = 0;
-
+    float friction_coefficient = 0;
+    float kinetic_force = 0;
+    float normal = 0;
+    bool pushing_left = false;
+    bool pushing_right = false;
 private:
     float gravity = -9.8f;
 };
